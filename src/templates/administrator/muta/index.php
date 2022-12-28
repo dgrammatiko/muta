2ccee821c8e8dff3f179987f53276a89363c840e
Brian Teeman <brian@teeman.net>
2022-12-28 16:35:25 +0000

    Update index.php

--- src/templates/administrator/muta/index.php
+++ src/templates/administrator/muta/index.php
@@ -13,24 +13,24 @@ $currentFileName = basename(__FILE__, '.php');
 
 /*
  * Set custom favicons
- * @see administrator/templates/muta/html/layouts/muta/favicons.php
+ * @see administrator/templates/muta/html/layouts/muta/partials/favicons.php
  */
 LayoutHelper::render('muta.partials.favicons', ['doc' => $this, 'params' => $this->params, 'entry' => $currentFileName]);
 
 /*
  * Set custom logos
- * @see administrator/templates/muta/html/layouts/muta/favicons.php
+ * @see administrator/templates/muta/html/layouts/muta/partials/logos.php
  */
 LayoutHelper::render('muta.partials.logos', ['doc' => $this, 'params' => $this->params, 'entry' => $currentFileName]);
 
 /*
  * Set the assets
- * @see administrator/templates/muta/html/layouts/muta/assets.php
+ * @see administrator/templates/muta/html/layouts/muta/partials/assets.php
  */
 LayoutHelper::render('muta.partials.assets', ['doc' => $this, 'params' => $this->params, 'entry' => $currentFileName]);
 
 /*
  * Render the HTML
- * @see administrator/templates/muta/html/layouts/muta/partials/login.php
+ * @see administrator/templates/muta/html/layouts/muta/entries/login.php
  */
 echo LayoutHelper::render('muta.entries.index', ['doc' => $this, 'params' => $this->params, 'entry' => $currentFileName]);
