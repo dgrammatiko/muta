fc16318ea3ec5b1cc47d37d8a3fa1b31fc0e695e
Dimitris Grammatikogiannis <dg@dgrammatiko.dev>
2023-10-21 13:35:26 +0300

    true webcomponent switcher

--- src/templates/administrator/muta/html/layouts/muta/entries/login.php
+++ src/templates/administrator/muta/html/layouts/muta/entries/login.php
@@ -13,13 +13,13 @@ use Joomla\CMS\Layout\LayoutHelper;
 extract($displayData);
 ?>
 <?= LayoutHelper::render('muta.partials.header', ['doc' => $doc, 'entry' => $entry, 'params' => $params, 'renderPosition' => $renderPosition]); ?>
-<div id="wrapper" class="wrapper flex-grow-1">
+<div id="wrapper" class="wrapper login-bg flex-grow-1">
   <div class="container-fluid container-main">
     <div class="login_message">
       <?= $renderPosition->message ?? ''; ?>
     </div>
     <main class="d-flex justify-content-center align-items-center h-100">
-      <div class="login">
+      <div class="card text-bg-light p-3 login">
         <div id="main-brand" class="main-brand">
           <h1 class="text-center"><?= $params->sitename; ?></h1>
           <h2 class="text-center"><?= Text::_('TPL_MUTA_BACKEND_LOGIN'); ?></h2>
